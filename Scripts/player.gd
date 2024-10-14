484f90fc420541ab98f404e6a8eabc2b9241aef4
Eden <edenprofessional@outlook.com>
2024-10-14 10:33:51 -0700

    added stuff

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
 #@onready var camera: camera2D = get_node("")
+@onready var char_animation: AnimatedSprite2D = self.get_node("AnimatedSprite2D")
 
 # character movement is smoother when a multiple of 60.
 const SPEED_MULTIPLIER = 3
 const SPEED = 60.0 * SPEED_MULTIPLIER
 const JUMP_VELOCITY = -400.0
+
+
 
 static var player = 1
 
@@ -14,18 +17,26 @@ var facing = ""
 
 #when a new player node is initialized, the player 
 func _init():
+	
 	if player == 1:
 		facing = "right"
+
 	else:
 		facing = "left"
+		char_animation.set_flip_h(false)
+		
+
+	#face_direction()
 
 	print(facing)
 	player += 1
 	print(player)
 
-	
-
-	
+func face_direction():
+	if facing == "right":
+		char_animation.flip_h = false
+	else:
+		char_animation.flip_h = true
 
 #takes in general action then changes to player specific action (ie: "move_left" -> "p1_move_left")
 func player_action(action):
