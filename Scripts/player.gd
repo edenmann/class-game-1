24c38316547f4d7ca004ccbcc276f5f72092c392
Eden <edenprofessional@outlook.com>
2024-11-05 09:58:46 -0800

    changed jumping, started on players jumping next to each other to other side

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 # character movement is smoother when a multiple of 60.
 const SPEED_MULTIPLIER = 3
 const SPEED = 60.0 * SPEED_MULTIPLIER
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -600.0
 
 static var static_player = 0
 var player: int
@@ -68,15 +68,17 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed(jump) and is_on_floor():
+	if Input.is_action_pressed(jump) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
+	
 	var direction := Input.get_axis(move_left, move_right)
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if is_on_floor():	
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 	
